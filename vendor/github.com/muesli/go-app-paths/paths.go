b02198d56c7c41c1d59a9e3e219a7244bb791ca3
Matti Ranta <matti@mdranta.net>
2018-08-12 16:39:39 -0400

    Vendor shiori

---
+++ vendor/github.com/muesli/go-app-paths/paths.go
@@ -0,0 +1,105 @@
+package apppaths
+
+import (
+	"errors"
+	"strings"
+
+	homedir "github.com/mitchellh/go-homedir"
+)
+
+var (
+	// ErrInvalidScope gets returned when an invalid scope type has been set.
+	ErrInvalidScope = errors.New("Invalid scope type")
+	// ErrRetrievingPath gets returned when the path could not be resolved.
+	ErrRetrievingPath = errors.New("Could not retrieve path")
+)
+
+// ScopeType specifies whether returned paths are user-specific or system-wide.
+type ScopeType int
+
+const (
+	// System is the system-wide scope.
+	System ScopeType = iota
+	// User is the user-specific scope.
+	User
+	// CustomHome uses a custom user home as scope.
+	CustomHome
+)
+
+// Scope holds scope & app-specific information.
+type Scope struct {
+	Type       ScopeType
+	CustomHome string
+	Vendor     string
+	App        string
+}
+
+// NewScope returns a new Scope that lets you query app- & platform-specific
+// paths.
+func NewScope(t ScopeType, vendor, app string) *Scope {
+	return &Scope{
+		Type:   t,
+		Vendor: vendor,
+		App:    app,
+	}
+}
+
+// NewCustomHomeScope returns a new Scope that lets you operate on a custom path
+// prefix.
+func NewCustomHomeScope(path, vendor, app string) *Scope {
+	return &Scope{
+		Type:       CustomHome,
+		CustomHome: path,
+		Vendor:     vendor,
+		App:        app,
+	}
+}
+
+// DataDir returns the full path to the application's data dir.
+func (s *Scope) DataDir() (string, error) {
+	p, err := s.dataDir()
+	if err != nil {
+		return p, err
+	}
+
+	return s.appendPaths(p), nil
+}
+
+// CacheDir returns the full path to the application's cache dir.
+func (s *Scope) CacheDir() (string, error) {
+	p, err := s.cacheDir()
+	if err != nil {
+		return p, err
+	}
+
+	return s.appendPaths(p), nil
+}
+
+// ConfigPath returns the full path to the application's config file.
+func (s *Scope) ConfigPath(filename string) (string, error) {
+	p, err := s.configDir()
+	if err != nil {
+		return p, err
+	}
+
+	return s.appendPaths(p, filename), nil
+}
+
+// LogPath returns the full path to the application's log file.
+func (s *Scope) LogPath(filename string) (string, error) {
+	p, err := s.logDir()
+	if err != nil {
+		return p, err
+	}
+
+	return s.appendPaths(p, filename), nil
+}
+
+// expandUser is a helper function that expands the first '~' it finds in the
+// passed path with the home directory of the current user.
+func expandUser(path string) string {
+	if u, err := homedir.Dir(); err == nil {
+		return strings.Replace(path, "~", u, -1)
+	}
+	return path
+}
