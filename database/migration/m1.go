2b9d347727209e4b2c89e9911ac56788817ed2fc
Matti Ranta <matti@mdranta.net>
2019-03-21 19:44:51 -0400

    simplify some structs

--- database/migration/m1.go
+++ database/migration/m1.go
@@ -9,10 +9,10 @@ import (
 )
 
 type M1Tag struct {
-	ID        int       `xorm:"'id' pk autoincr" json:"id"`
-	Name      string    `json:"name"`
-	Deleted   bool      `json:"-"`
-	NBookmark int       `xorm:"n_bookmarks" json:"nBookmarks"`
+	ID        int `xorm:"'id' pk autoincr"`
+	Name      string
+	Deleted   bool
+	NBookmark int       `xorm:"n_bookmarks"`
 	Created   time.Time `xorm:"created"`
 	Updated   time.Time `xorm:"updated"`
 }
@@ -22,18 +22,18 @@ func (m M1Tag) TableName() string {
 }
 
 type M1Bookmark struct {
-	ID          int       `xorm:"'id' pk autoincr" json:"id"`
-	URL         string    `xorm:"url" json:"url"`
-	Title       string    `xorm:"'title' NOT NULL" json:"title"`
-	ImageURL    string    `xorm:"'image_url' NOT NULL" json:"imageURL"`
-	Excerpt     string    `xorm:"'excerpt' NOT NULL" json:"excerpt"`
-	Author      string    `xorm:"'author' NOT NULL" json:"author"`
-	MinReadTime int       `xorm:"'min_read_time' DEFAULT 0"   json:"minReadTime"`
-	MaxReadTime int       `xorm:"'max_read_time' DEFAULT 0"   json:"maxReadTime"`
-	Modified    time.Time `xorm:"modified"    json:"modified"`
-	Content     string    `xorm:"TEXT 'content'" json:"content"`
-	HTML        string    `xorm:"TEXT 'html'" json:"html,omitempty"`
-	HasContent  bool      `xorm:"has_content" json:"hasContent"`
+	ID          int       `xorm:"'id' pk autoincr"`
+	URL         string    `xorm:"url"`
+	Title       string    `xorm:"'title' NOT NULL"`
+	ImageURL    string    `xorm:"'image_url' NOT NULL"`
+	Excerpt     string    `xorm:"'excerpt' NOT NULL"`
+	Author      string    `xorm:"'author' NOT NULL"`
+	MinReadTime int       `xorm:"'min_read_time' DEFAULT 0"`
+	MaxReadTime int       `xorm:"'max_read_time' DEFAULT 0"`
+	Modified    time.Time `xorm:"modified"`
+	Content     string    `xorm:"TEXT 'content'"`
+	HTML        string    `xorm:"TEXT 'html'"`
+	HasContent  bool      `xorm:"has_content"`
 	Created     time.Time `xorm:"created"`
 	Updated     time.Time `xorm:"updated"`
 }
@@ -52,9 +52,9 @@ func (m M1BookmarkTag) TableName() string {
 }
 
 type M1Account struct {
-	ID       int       `xorm:"'id' pk autoincr" json:"id"`
-	Username string    `json:"username"`
-	Password string    `json:"password"`
+	ID       int `xorm:"'id' pk autoincr"`
+	Username string
+	Password string
 	Created  time.Time `xorm:"created"`
 	Updated  time.Time `xorm:"updated"`
 }
